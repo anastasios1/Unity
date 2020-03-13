4ee3e2f4fe9db755c452364bb73d9b339d094a81
Anastasios Vulgaridis <a.voulgaridis@student.tugraz.at>
2020-03-13 15:21:13 +0100

    AR Towers of HANOI

---
+++ HanoiTowersAR/Assets/Script/FlashingObject.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingObject : MonoBehaviour {
+
+    
+     int redCol, greenCol, blueCol;
+
+    public bool lookingAtObject = false;
+    public bool flashingIn = true;
+    public bool startedFlashing = false;
+
+    Color32 originalColor;
+
+    public GameManagerSelection selectedObject;
+	// Use this for initialization
+	void Start () {
+
+        originalColor = this.transform.GetComponent<Renderer>().material.color;
+        selectedObject = GameObject.FindObjectOfType<GameManagerSelection>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (selectedObject.selected != null)
+        {
+            lookingAtObject = true;
+            if (selectedObject.selected == this.transform.gameObject)
+            {
+                
+               selectedObject.selected.transform.GetComponent<Renderer>().material.color = new Color32((byte)redCol, (byte)greenCol, (byte)blueCol, 255);
+                if (startedFlashing == false)
+                {
+                    startedFlashing = true;
+                    StartCoroutine(FlashObject());
+                    
+                }
+            }
+            else
+            {
+                this.transform.GetComponent<Renderer>().material.color = originalColor;
+            }
+        }
+        else
+        {
+            this.transform.GetComponent<Renderer>().material.color = originalColor;
+            startedFlashing = false;
+            lookingAtObject = false;
+            StopCoroutine(FlashObject());
+            
+        }
+    }
+
+    IEnumerator FlashObject()
+    {
+        while(lookingAtObject==true)
+        {
+            yield return new WaitForSeconds(0.05f);
+            if(flashingIn==true)
+            {
+                if(blueCol<=30)
+                {
+                    flashingIn = false;
+                }
+                else
+                {
+                    blueCol -= 25;
+                    redCol -= 25;
+                }
+            }
+            if (flashingIn == false)
+            {
+
+                if (blueCol >= 250)
+                {
+                    flashingIn = true;
+                }
+                else
+                {
+                    blueCol += 25;
+                    redCol += 25;
+                }
+            }
+        }
+    }
+	
+}
---
+++ HanoiTowersAR/Assets/Script/GameManagerSelection.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Vuforia;
+using System;
+using UnityEngine.UI;
+
+
+public class GameManagerSelection : MonoBehaviour, IVirtualButtonEventHandler
+{
+    
+    
+
+    public GameObject btn1;
+    public GameObject btn2;
+    public GameObject btn3;
+
+    public GameObject towerSelected;
+
+    public GameObject selected;
+
+    public GameObject tower1;
+    public GameObject tower2;
+    public GameObject tower3;
+
+    public LayerMask ignoreLayer;
+
+    public Text warning;
+
+    //Vector3 currentPos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+       
+        btn1 = GameObject.Find("btn1");
+        btn2 = GameObject.Find("btn2");
+        btn3 = GameObject.Find("btn3");
+
+        btn1.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        btn2.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        btn3.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selected == null)
+            warning.text = "Select A Disc";
+        else
+            warning.text = "";
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000.0f, ~ignoreLayer))
+            {
+                
+                GameObject hitObject;
+                if (hit.transform.IsChildOf(transform))
+                    hitObject = hit.transform.parent.gameObject;
+                else
+                    hitObject = hit.collider.gameObject;
+
+                selected = hitObject;
+                Debug.Log("Unit Selected:" + selected.name);
+                
+
+              //  Selected(selected);
+            }
+            else
+            {
+                if (selected != null)
+                {
+                    
+                    selected = null;
+                    Debug.Log("Diselect Unit");
+                    
+                }
+            }
+        }
+
+    }
+    void Selected(GameObject obj)
+    {
+        if (towerSelected != null)
+        {
+            warning.text = "";
+            selected = obj;
+            Vector3 cPo = obj.transform.position;
+            RaycastHit hit;
+            if (Physics.Raycast(obj.transform.position, Vector3.up, out hit, 1000.0f, ~ignoreLayer))
+            {
+                if (hit.collider != null)
+                    Debug.Log("There is another object above");
+            }
+            else
+            {
+                warning.text = "Please Select A Disc";
+                Vector3 upDir = new Vector3(0.0f, 10.0f, 0.0f);
+                obj.transform.position = towerSelected.transform.position + upDir;
+
+                if (Physics.Raycast(selected.transform.position, -Vector3.up, out hit, 1000.0f, ~ignoreLayer))
+                {
+                    if (selected.transform.localScale.x > hit.collider.transform.localScale.x)
+                    {
+
+                        {
+                            Debug.Log("There is another object there");
+                            obj.transform.position = cPo;
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+
+    }
+
+
+
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+
+
+        if (vb.name == btn1.name)
+            towerSelected = tower1;
+        if (vb.name == btn2.name)
+            towerSelected = tower2;
+        if (vb.name == btn3.name)
+            towerSelected = tower3;
+
+        Debug.Log("Tower Selected :" + towerSelected.name);
+        Selected(selected);
+
+
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+
+    }
+
+    
+   
+        
+    
+
+   
+
+
+}
